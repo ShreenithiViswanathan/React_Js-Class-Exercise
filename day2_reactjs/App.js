c894d2f84c5173276c81b703784b2da10834a3ce
Shreenithi Vishwanathan <shreenithiviswanathan@gmail.com>
2024-09-02 14:34:28 +0530

    Add files via upload

---
+++ day2_reactjs/App.js
@@ -0,0 +1,59 @@
+//day-1_ce
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+//day-2_ce
+import React from "react";
+
+function Day2()
+{
+    const styleOfDiv ={
+        backgroundColor: "lightblue",
+        color:"blue",
+        padding:"10px",
+        border:"1px solid blue",
+        borderRadius:"5px"
+    };
+    const styleOfP ={
+                color:"darkBlue",
+                fontSize:"16px"
+    };
+    return(
+        <>
+        <div className="exerxise1"><h1>Hello World!</h1></div>
+        <div className="exerxise2">
+            <h1>Inline Style in JSX Example.</h1>
+            <div style={styleOfDiv}>
+                <p style={styleOfP}>This is a paragraph with inline styles applied</p>
+            </div>
+            
+        </div>
+        
+        </>
+    )
+
+}
+
+export default Day2;
